73182f11a3ead880c04d178596e94d3a33e2cc3a
윤영훈 <wodlxosxos73@gmail.com>
2021-07-28 15:22:23 +0900

    complete `visitNumberDB`

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import styled from 'styled-components';
 import Layout from '../../components/Layout/Layout';
 import Sidebar from '../../components/Layout/Sidebar';
 import Content from '../../components/Layout/Content';
 import Card from '../../components/Layout/Card';
-
+import { useLocation, useHistory } from 'react-router-dom';
 import Surfing from './Surfing';
 import TodayState from './TodayState';
+import { firestore } from '../../fBase';
 
 import {
   MdLink,
@@ -60,10 +61,10 @@ const ContentSection = styled.section`
   }
   .profImg {
     position: absolute;
-    width: 12%;
-    height: auto;
-    right: 20%;
-    top: 30%;
+    width: 4%;
+    height: 8%;
+    right: 24%;
+    top: 46%;
     cursor: pointer;
   }
 `;
@@ -137,8 +138,42 @@ const LinkTitle = styled.p`
     color: ${props => props.theme.mainColor.color};
   }
 `;
-
 const Home = () => {
+  const location = useLocation()
+  let today = ['-', '-']
+  const [todayInfo, setTodayInfo] = useState(today)
+  const dateInfo = parseInt(Date().split(' ')[2])
+  useEffect(() => {
+    firestore.collection('users').get().then(docs => {
+      docs.forEach(doc => {
+        if (location.state.curLogin === doc.data().id) {
+          if (dateInfo === doc.data().last) {
+            today = [doc.data().today[0] + 1, doc.data().today[1] + 1]
+          } else {
+            today = [1, doc.data().today[1] + 1]
+          }
+          console.log(today)
+          firestore.doc(`users/${location.state.curLogin}`).update({
+            today: today
+          }).then(function () {
+            console.log(1)
+          }).catch(function (error) {
+            console.log('error', error)
+          })
+          firestore.doc(`users/${location.state.curLogin}`).update({
+            last: dateInfo
+          }).then(function () {
+            console.log(1)
+          }).catch(function (error) {
+            console.log('error', error)
+          })
+        }
+      })
+    }).then(tmp => {
+      console.log(dateInfo)
+      setTodayInfo(today)
+    })
+  }, []);
   const goGithub = () => {
     window.location.href = 'https://github.com/danbiilee/react-miniportfoly';
   };
@@ -148,15 +183,15 @@ const Home = () => {
 
   return (
     <Layout>
-      <Sidebar>
+      <Sidebar todayInfo={todayInfo}>
         <Card>
           <FlexWrapper>
             <ProfileSection>
               <TodayState />
               <ProfileImg>
-               <img src={publicUrl + '/resources/img/character.png'} alt="profile" />
+                <img src={publicUrl + '/resources/img/character.png'} alt="profile" />
               </ProfileImg>
-              
+
               <LinkTitle onClick={goGithub}>
                 <MdLink />
                 Github
@@ -202,9 +237,9 @@ const Home = () => {
               </img>
               <a href="/react-miniportfoly/profile">
                 <img
-                className='profImg'
-                src={publicUrl + '/resources/img/mProfile.png'}
-                /> 
+                  className='profImg'
+                  src={publicUrl + '/resources/img/profile.jpg'}
+                />
               </a>
             </div>
           </ContentSection>
